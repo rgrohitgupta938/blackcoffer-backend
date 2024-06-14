adfc3d8ffd2ce7b3a67ab47031add2294ad5050d
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-06-14 11:00:14 +0530

    Cors

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const mongoose = require("mongoose");
 const fs = require("fs");
 const path = require("path");
 const Company = require("./models/company");
-const cors = require("cors");
+
 
 const app = express();
-app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, X-Response-Time"
+  );
+  next();
+});
 app.use(express.json());
 
 mongoose
